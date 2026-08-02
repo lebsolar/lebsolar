e9fcfcc4293c0ab849d67a7a287a243e06d39d25
Santiago Ramírez Pérez <santiagoramirezperez96@gmail.com>
2026-08-01 20:29:58 -0500

    Banner página de inicio

---
+++ src/app/components/navbar/navbar.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from "@angular/router";
+
+
+@Component({
+  selector: 'app-navbar',
+  imports: [CommonModule, RouterLink],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+})
+export class Navbar {
+  menuAbierto = false;
+  toggleMenu() {
+    this.menuAbierto = !this.menuAbierto;
+  }
+
+}
