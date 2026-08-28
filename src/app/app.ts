71c314f8b34673b6248712bb9a1d5ce0e2b41703
Santiago Ramírez Pérez <santiagoramirezperez96@gmail.com>
2026-08-27 20:53:10 -0500

    Footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,14 @@ import { Component, signal } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 
 import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    Navbar
+    Navbar,
+    Footer,
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,14 +1,75 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
+import emailjs from '@emailjs/browser';
 
 @Component({
-  selector: 'app-contact',
-  imports: [],
+  selector: 'app-contacto',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
 export class Contact {
+
   constructor(private title: Title) {
     this.title.setTitle('CONTACTO | LebSolar Ingeniería');
+  }
+
+  loading = false;
+  enviado = false;
+  error = false;
+
+  autoResize(event: any) {
+    const textarea = event.target;
+    textarea.style.height = 'auto';
+    textarea.style.overflow = 'hidden';
+    textarea.style.height = textarea.scrollHeight + 'px';
+  }
+
+  enviarFormulario(form: any) {
+
+    if (form.invalid) return;
+
+    this.loading = true;
+    this.enviado = false;
+    this.error = false;
+
+    emailjs.send(
+      'SERVICE_ID',
+      'TEMPLATE_ID',
+      {
+        from_name: form.value.nombre,
+        phone: form.value.telefono,
+        from_email: form.value.email || 'No proporcionado',
+        message: form.value.mensaje || 'Sin mensaje',
+        servicio: form.value.servicio || 'No especificado'
+      },
+      '' //Public Key
+    )
+      .then(() => {
+
+        this.loading = false;
+        this.enviado = true;
+
+        form.resetForm();
+
+        setTimeout(() => {
+          this.enviado = false;
+        }, 4000);
+
+      })
+      .catch((err) => {
+
+        console.error('Error EmailJS:', err);
+        this.loading = false;
+        this.error = true;
+
+        setTimeout(() => {
+          this.error = false;
+        }, 4000);
+
+      });
   }
 }
